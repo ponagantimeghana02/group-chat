fc7382edf4f7d33e157021fc730efb2a6fac3a40
Ponaganti Meghana <ponagantimeghana02@gmail.com>
2026-02-16 12:43:54 +0530

    modified

--- public/group.js
+++ public/group.js
@@ -1,68 +1,95 @@
 const socket = io();
 let currentUser = "";
-let selectedUser = "";
+let selectedUser = null;
+const privateChats = {};
 
 function addUser() {
   let uId = document.getElementById("userId").value;
   currentUser = uId;
-  document.getElementById("userName").innerText = "Username: " + uId;
+  document.getElementById("userName").innerText ="Group Chat";
   socket.emit("sendNameToServer", uId);
   document.getElementById("userId").value = "";
 }
 
-socket.on("sendUsersDataToClient", (user) => {
+socket.on("sendUsersDataToClient", (users) => {
   let usersArray = document.getElementById("usersList");
-  usersArray.innerHTML = "";
-
-  let listOfUsers = user
-    .map(
-      (u) => `<li onclick="selectUser('${u.username}')">${u.username}</li>`
-    )
-    .join("");
-
-  usersArray.innerHTML = listOfUsers;
+  usersArray.innerHTML = `<li style="font-weight:bold;color:green">You</li>
+${users
+  .filter(u => u.username !== currentUser)
+  .map((u) => `<li onclick="selectUser('${u.username}', '${u.socketId}')">${u.username}</li>`)
+  .join("")}`;
 });
 
-function selectUser(name) {
-  selectedUser = name;
-  document.getElementById("chatWith").innerText = name;
+function selectUser(username, socketId) {
+  selectedUser = { username, socketId };
+  document.getElementById("userName").innerText =
+    "You are chatting with " + username;
+  const box = document.getElementById("displayingMessagesOfUsers");
+  box.innerHTML = "";
+  (privateChats[username] || []).forEach((m) => {
+    addMsg(m.from, m.msg, m.from === "You");
+  });
 }
 
 function broadCasteMsg() {
-  let message = document.getElementById("textMessage").value;
-
+  let msg = document.getElementById("textMessage").value;
+  if (!msg) return;
+  if (selectedUser) {
+    socket.emit("sendPrivateChatToServer", {
+      msg,
+      from: currentUser,
+      toSocketId: selectedUser.socketId,
+    });
+    if (!privateChats[selectedUser.username])
+      privateChats[selectedUser.username] = [];
+    privateChats[selectedUser.username].push({ from: "You", msg });
+    addMsg("You", msg, true);
+  } else {
     socket.emit("sendUserChatToServer", {
-      msg: message,
+      msg,
       userName: currentUser,
     });
-  
+  }
   document.getElementById("textMessage").value = "";
 }
 
-socket.on("sendGroupDataToClient", (obj) => {
-  let displayingDataOfGroup = document.getElementById(
-    "displayingMessagesOfUsers"
-  );
+socket.on("sendGroupDataToClient", (arr) => {
+  if (selectedUser) return;
 
-  displayingDataOfGroup.innerHTML = obj
-    .map((data) => {
-      const displayName = data.userName == currentUser ? "You" : data.userName;
-      return `<p><b>${displayName}:</b> ${data.msg}</p>`;
-    })
-    .join("");
+  const box = document.getElementById("displayingMessagesOfUsers");
+  box.innerHTML = "";
+
+  arr.forEach((data) => {
+    const name = data.userName === currentUser ? "You" : data.userName;
+    addMsg(name, data.msg);
+  });
+});
+
+socket.on("sendPrivateDataToClient", ({ from, msg }) => {
+  if (!privateChats[from]) privateChats[from] = [];
+  privateChats[from].push({ from, msg });
+  if (selectedUser && selectedUser.username === from) {
+    addMsg(from, msg, false);
+  }
+});
+
+function addMsg(name, msg) {
+  const box = document.getElementById("displayingMessagesOfUsers");
+
+  const div = document.createElement("div");
+  const isMe = name === "You" || name === currentUser;
+
+  div.className = "msg " + (isMe ? "me" : "other");
+  if (!selectedUser && !isMe) {
+    div.innerHTML = `<b>${name}</b><br>${msg}`;
+  } else if (!selectedUser && isMe) {
+    div.innerHTML = `<b>You</b><br>${msg}`;
+  } else {
+    div.innerText = msg;
+  }
+
+  box.appendChild(div);
 
   const container = document.getElementById("rightPanelUpContent");
   container.scrollTop = container.scrollHeight;
-});
-
-socket.on("sendPrivateDataToClient", (obj) => {
-  let displayingDataOfGroup = document.getElementById(
-    "displayingMessagesOfUsers"
-  );
-
-  const displayName = obj.from == currentUser ? "You" : obj.from;
-  displayingDataOfGroup.innerHTML += `<p><b>${displayName}:</b> ${obj.msg}</p>`;
-
-  const container = document.getElementById("rightPanelUpContent");
-  container.scrollTop = container.scrollHeight;
-});
+}
